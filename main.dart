09b3afc068358f47220e78f770132706322b69d9
pdr-tuche <55400486+pdr-tuche@users.noreply.github.com>
2023-08-31 02:54:11 +0000

    primeiro commit

--- main.dart
+++ main.dart
@@ -1,3 +1,38 @@
+import 'dart:io';
+
 void main() {
-  print('Hello World!');
+    print('digite o primeiro numero');
+    var num1 = double.parse(stdin.readLineSync()!);
+    print('digite o segundo numero');
+    var num2 = double.parse(stdin.readLineSync()!);
+    print('digite uma operação');
+    String operacao = stdin.readLineSync()!;
+
+    double resultado = 0;
+
+  switch (operacao) {
+    case "+":
+      resultado = num1 + num2;
+      break;
+    case "-":
+      resultado = num1 - num2;
+      break;
+    case "*":
+      resultado = num1 * num2;
+      break;
+    case "/":
+      if (num2 != 0) {
+        resultado = num1 / num2;
+      } else {
+        print("Não é possível dividir por zero.");
+        return;
+      }
+      break;
+    default:
+      print("Operação inválida.");
+      return;
+  }
+
+  print("O resultado da operação é: $resultado");
+
 }
